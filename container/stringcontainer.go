d039aca987885293b35a76a7191806866531823c
milittle <mizeshuang@gmail.com>
2021-07-27 00:26:52 +0800

    change the contailner template space

--- container/stringcontainer.go
+++ container/stringcontainer.go
@@ -3,12 +3,15 @@ package container
 type StringContainer struct {
 	s []string
 }
+
 func NewStringContainer() *StringContainer {
 	return &StringContainer{s: []string{}}
 }
+
 func (c *StringContainer) Put(val string) {
 	c.s = append(c.s, val)
 }
+
 func (c *StringContainer) Get() string {
 	r := c.s[0]
 	c.s = c.s[1:]
