8b6828a2e4951254389461e731ab8c30885d7500
milittle <mizeshuang@gmail.com>
2021-12-08 11:33:59 +0800

    add list reverse

--- leetcode/0206.go
+++ leetcode/0206.go
@@ -1,1 +1,14 @@
 package leetcode
+
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	curr := head
+
+	for curr != nil {
+		next := curr.Next
+		curr.Next = pre
+		pre = curr
+		curr = next
+	}
+	return pre
+}
